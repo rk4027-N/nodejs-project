731b1776cebbdce42d1ed0d6760dbaf0e0de6336
Rajkumar <gudalarajkumar4444@gmail.com>
2025-07-05 19:47:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // index.js
 const express = require('express');
 const app = express();
-const port = 3001;
+const port = 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello from Node.js App!');
